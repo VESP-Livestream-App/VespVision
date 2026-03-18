a4afb63aef2b0cef3cf488b70a4d6b936e5af9c3
Kelm <lkevin2003@gmail.com>
2026-03-17 23:04:23 -0700

    revmoeed fw calls for peripherals

--- firmware/Tripod/Tripod.ino
+++ firmware/Tripod/Tripod.ino
@@ -21,8 +21,8 @@ moveCmd_u moveCmd =
 };
 
 BLEDevice central;
-static unsigned long lastSampleMs = 0;
-static unsigned long lastDisplayMs = 0;
+// static unsigned long lastSampleMs = 0;
+// static unsigned long lastDisplayMs = 0;
 
 // forward declarations
 void setServo(int angle, unsigned long durationMs);
@@ -49,20 +49,20 @@ void loop() {
     }
   }
   
-  unsigned long now = millis();
-  float soc_pct = -1.0f;
+  // unsigned long now = millis();
+  // float soc_pct = -1.0f;
 
-  if (now - lastSampleMs >= 1000) 
-  {
-    lastSampleMs += 1000;
-    soc_pct = calculateDisplayData();
-  }
+  // if (now - lastSampleMs >= 1000) 
+  // {
+  //   lastSampleMs += 1000;
+  //   soc_pct = calculateDisplayData();
+  // }
 
-  if (now - lastDisplayMs >= 1000) 
-  {
-    lastDisplayMs += 1000;
-    updateDisplay();
-  }
+  // if (now - lastDisplayMs >= 1000) 
+  // {
+  //   lastDisplayMs += 1000;
+  //   updateDisplay();
+  // }
   
   // If connected, handle commands
   if (central && central.connected()) 
@@ -75,12 +75,12 @@ void loop() {
     updateServo();
     int servoAngle = getServoAngle();
     servoAngleBroadcast(servoAngle);
-    if (soc_pct >= 0.0f && soc_pct <= 100.0f) {
-      tripodSocBroadcast(soc_pct);
-    }
-    else {
-      Serial.println("SOC out of range, not broadcasting");
-    }
+    // if (soc_pct >= 0.0f && soc_pct <= 100.0f) {
+    //   tripodSocBroadcast(soc_pct);
+    // }
+    // else {
+    //   Serial.println("SOC out of range, not broadcasting");
+    // }
   } 
   else if (central) 
   {
